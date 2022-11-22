f84de12affd3a9f3603817ad08cee6d76261316a
Nethan123 <regijose@gmail.com>
2022-11-22 17:22:17 -0500

    pieces can kill

--- src/scripts/movePiece.js
+++ src/scripts/movePiece.js
@@ -2,17 +2,25 @@ let prevTile;
 
 document.querySelectorAll("tile").forEach(element => 
     element.addEventListener("click", e => {
-        if(!prevTile && e.target.style.length > 0) prevTile = e.target;
+        if(!prevTile && e.target.style.length > 0) return prevTile = e.target;
 
-        if(prevTile && !e.target.style.length > 0){
+        if(prevTile){
             const classes = e.target.className.split(" ");
             const prevClasses = prevTile.className.split(" ");
-
-            addPiece(classes[0], prevClasses[2].slice(0, 5), prevClasses[2].slice(6, 15));
-
+    
+            //Remove piece from first tile
             prevTile.style.backgroundImage = "none";
             prevTile.style.removeProperty("background-image");
             prevTile.className = prevClasses[0] + " " + prevClasses[1];
+    
+            //Remove piece from second tile (if any)
+            e.target.style.backgroundImage = "none";
+            e.target.style.removeProperty("background-image");
+            e.target.className = classes[0] + " " + classes[1];
+    
+            //Add piece on first tile to second tile
+            addPiece(classes[0], prevClasses[2].slice(0, 5), prevClasses[2].slice(6, 15));
+
             prevTile = undefined;
         }
     })
